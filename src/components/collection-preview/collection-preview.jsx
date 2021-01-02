e9c0a4484eb102c80200fc328b442e31937d029f
Roumesh Persand <rpersand25@gmail.com>
2021-01-02 14:58:25 -0500

    added addItem onclick function to add item to cart button in cart and added quantity property to item object to properly group items in cart

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.jsx';
 
-import './collection-preview.scss'
+import './collection-preview.scss';
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((item, idx) => idx < 4).map(({ id, ...otherItemProps }) => (
-                <CollectionItem key={id} {...otherItemProps} />
+            {items.filter((item, idx) => idx < 4).map((item) => (
+                <CollectionItem key={item.id} item={item} />
             ))}
         </div>
     </div>
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,10 +1,12 @@
 const initialState = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 // action type
 
 const TOGGLE_CART_HIDDEN = 'TOGGLE_CART_HIDDEN';
+const ADD_ITEM = 'ADD_ITEM';
 
 // action creator
 
@@ -12,12 +14,35 @@ export const toggleCartHidden = () => ({
     type: TOGGLE_CART_HIDDEN
 });
 
+export const addItem = (item) => ({
+    type: ADD_ITEM,
+    payload: item
+})
+
+const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(item => item.id === cartItemToAdd.id);
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        )
+    }
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+}
+
 const cartReducer = (state = initialState, action) => {
     switch (action.type) {
         case TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state
