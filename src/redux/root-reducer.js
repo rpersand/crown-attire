445618abffb7182a4e04629464a1f3aad31168a7
Roumesh Persand <rpersand25@gmail.com>
2021-01-03 20:55:20 -0500

    incorporated redux-persist into app so that state is persistent

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
 
-export default combineReducers({
+// key is from where do you want persist to begin.
+// whitelist is which reducers do you want to persist. we don't need to add user because
+// user persistance is done by firebase auth
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
